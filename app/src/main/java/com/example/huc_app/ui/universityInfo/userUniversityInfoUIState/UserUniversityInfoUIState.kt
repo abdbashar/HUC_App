05b2f4fda9298ff2737b55fd4f92ee99b36f8fe9
abdbashar <abdullahbashar98@gmail.com>
2023-11-06 19:53:59 +0300

    Created UserUniversityInfoUIState data class to display university information in the app's UI.

---
+++ app/src/main/java/com/example/huc_app/ui/universityInfo/userUniversityInfoUIState/UserUniversityInfoUIState.kt
@@ -0,0 +1,10 @@
+package com.example.huc_app.ui.universityInfo.userUniversityInfoUIState
+
+data class UserUniversityInfoUIState(
+    val department: String = "",
+    val classType: String = "",
+    val startDate: String = "",
+    val window: String = "",
+    val email: String = "",
+    val universityNumber: String = "",
+)
