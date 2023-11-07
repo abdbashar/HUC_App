082655482e6c880ba23cc3616d51fe2af2efe565
abdbashar <abdullahbashar98@gmail.com>
2023-11-07 21:53:00 +0300

    Implemented the UserUniversityInfo domain data class to represent a user's university information.

---
+++ app/src/main/java/com/example/huc_app/domain/models/UserUniversityInfo.kt
@@ -0,0 +1,10 @@
+package com.example.huc_app.domain.models
+
+data class UserUniversityInfo(
+    val department: String,
+    val classType: String,
+    val email: String,
+    val startDate: String,
+    val universityNumber: String,
+    val window: String,
+)
