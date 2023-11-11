263d9e085fa6dda5e46edfd22188b835926a947c
abdbashar <abdullahbashar98@gmail.com>
2023-11-11 16:25:51 +0300

    Created UniversityNewsDetailsDTO to represent university news details from the remote data source

---
+++ app/src/main/java/com/example/huc_app/data/remote/response/UniversityNewsDetailsDTO.kt
@@ -0,0 +1,8 @@
+package com.example.huc_app.data.remote.response
+
+data class UniversityNewsDetailsDTO(
+    val date: String,
+    val default_img: String,
+    val details: String,
+    val title: String
+)
