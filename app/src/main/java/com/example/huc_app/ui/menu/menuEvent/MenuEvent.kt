3558aae4a006c7da480445f116bc9637e26cbd7c
abdbashar <abdullahbashar98@gmail.com>
2023-11-16 13:59:11 +0300

    Implement toEvent() extension function for MenuEvent

--- app/src/main/java/com/example/huc_app/ui/menu/menuEvent/MenuEvent.kt
+++ app/src/main/java/com/example/huc_app/ui/menu/menuEvent/MenuEvent.kt
@@ -1,8 +1,11 @@
 package com.example.huc_app.ui.menu.menuEvent
 
+import com.example.huc_app.util.Event
+
 sealed class MenuEvent {
     object PersonalInfoClicked : MenuEvent()
     object SecondarySchoolInfoClicked : MenuEvent()
     object UniversityInfoClicked : MenuEvent()
 }
 
+fun MenuEvent.toEvent(): Event<MenuEvent> = Event(this)
