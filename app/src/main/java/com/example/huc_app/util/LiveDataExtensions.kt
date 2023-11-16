aeb6448edd9b8c53d18147315e031c56437ff49e
abdbashar <abdullahbashar98@gmail.com>
2023-11-16 13:17:23 +0300

    Add observeEvent extension function for LiveData

--- app/src/main/java/com/example/huc_app/util/LiveDataExtensions.kt
+++ app/src/main/java/com/example/huc_app/util/LiveDataExtensions.kt
@@ -1,7 +1,16 @@
 package com.example.huc_app.util
 
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
 fun <T> MutableLiveData<Event<T>>.postEvent(content: T) {
     postValue(Event(content))
+}
+
+inline fun <T> LiveData<Event<T>>.observeEvent(
+    owner: LifecycleOwner,
+    crossinline onEventUnhandledContent: (T) -> Unit
+) {
+    observe(owner) { it?.getContentIfNotHandled()?.let(onEventUnhandledContent) }
 }
