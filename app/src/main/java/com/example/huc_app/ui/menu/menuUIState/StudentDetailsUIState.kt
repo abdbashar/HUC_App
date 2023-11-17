f314760abcd92a173e07e5e871cea1a231b8e78d
abdbashar <abdullahbashar98@gmail.com>
2023-11-17 20:53:35 +0300

    Created StudentDetailsUIState to display student info in the app's UI

---
+++ app/src/main/java/com/example/huc_app/ui/menu/menuUIState/StudentDetailsUIState.kt
@@ -0,0 +1,7 @@
+package com.example.huc_app.ui.menu.menuUIState
+
+data class StudentDetailsUIState(
+    var email: String = "",
+    var fullName: String = "",
+    var personalPicture: String = "",
+)
