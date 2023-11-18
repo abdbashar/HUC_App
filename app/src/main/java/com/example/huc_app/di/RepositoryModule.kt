f815e57fd0aad88da7d55ff8d172ac92cb373b24
abdbashar <abdullahbashar98@gmail.com>
2023-11-18 11:42:56 +0300

    Add binding of AuthRepository to AuthRepositoryImpl in RepositoryModule

--- app/src/main/java/com/example/huc_app/di/RepositoryModule.kt
+++ app/src/main/java/com/example/huc_app/di/RepositoryModule.kt
@@ -1,12 +1,18 @@
 package com.example.huc_app.di
 
 import com.example.huc_app.data.repository.*
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
 @InstallIn(ViewModelComponent::class)
 abstract class RepositoryModule {
+
+    @ViewModelScoped
+    @Binds
+    abstract fun bindAuthRepository(authRepositoryImpl: AuthRepositoryImp): AuthRepository
 
 }
