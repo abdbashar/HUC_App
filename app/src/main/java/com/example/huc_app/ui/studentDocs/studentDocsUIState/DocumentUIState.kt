6499a2b3a9b6f67f38cb8e8d85d3cf56d5e6bea6
abdbashar <abdullahbashar98@gmail.com>
2023-11-21 21:09:16 +0300

    Created DocumentUIState to represent document details in the app's UI

---
+++ app/src/main/java/com/example/huc_app/ui/studentDocs/studentDocsUIState/DocumentUIState.kt
@@ -0,0 +1,8 @@
+package com.example.huc_app.ui.studentDocs.studentDocsUIState
+
+data class DocumentUIState(
+    val documentUrl: String = "",
+    val documentName: String = "",
+    val thumbnailDoc: String = "",
+    val pageCount: Int = 0,
+)
