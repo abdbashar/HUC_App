6a14d53ad72e39beecc786959b7ff4af1e4b59ee
abdbashar <abdullahbashar98@gmail.com>
2023-11-22 21:53:37 +0300

    Implement showPopupMenu in StudentDocsAdapter for handling kebab menu click, enabling document opening and downloading

--- app/src/main/java/com/example/huc_app/ui/studentDocs/StudentDocsAdapter.kt
+++ app/src/main/java/com/example/huc_app/ui/studentDocs/StudentDocsAdapter.kt
@@ -1,6 +1,10 @@
 package com.example.huc_app.ui.studentDocs
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.PopupWindow
 import com.example.huc_app.R
 import com.example.huc_app.ui.base.BaseAdapter
 import com.example.huc_app.ui.base.BaseInteractionListener
@@ -18,6 +22,29 @@ class StudentDocsAdapter(
         holder.itemView.findViewById<View>(R.id.kebabMenu).setOnClickListener { view ->
             showPopupMenu(view, getItemAt(position))
         }
+    }
+
+    private fun showPopupMenu(anchorView: View, item: DocumentUIState) {
+        val inflater = anchorView.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val popupView = inflater.inflate(R.layout.dialog_option_menu, null)
+
+        val width = LinearLayout.LayoutParams.WRAP_CONTENT
+        val height = LinearLayout.LayoutParams.WRAP_CONTENT
+
+        val popupWindow = PopupWindow(popupView, width, height, true)
+
+        popupView.findViewById<View>(R.id.open_doc).setOnClickListener {
+             listener.onOpenDoc(item)
+            popupWindow.dismiss()
+        }
+
+        popupView.findViewById<View>(R.id.download_doc).setOnClickListener {
+            listener.onDownloadDoc(item)
+            popupWindow.dismiss()
+        }
+        val offsetX = 0
+        val offsetY = -anchorView.height + 200
+        popupWindow.showAsDropDown(anchorView, offsetX, offsetY)
     }
 }
 
