1bb14c4ac88d2df6d940364c46cb90b8906ccc9d
abdbashar <abdullahbashar98@gmail.com>
2023-11-22 21:48:21 +0300

    Extend StudentDocsClicksListener with onDownloadDoc method for handling document download

--- app/src/main/java/com/example/huc_app/ui/studentDocs/StudentDocsAdapter.kt
+++ app/src/main/java/com/example/huc_app/ui/studentDocs/StudentDocsAdapter.kt
@@ -17,4 +17,5 @@ class StudentDocsAdapter(
 interface StudentDocsClicksListener : BaseInteractionListener {
     fun onListClick(item: DocumentUIState)
     fun onOpenDoc(item: DocumentUIState)
+    fun onDownloadDoc(item: DocumentUIState)
 }
