9159ebd18c4d2b45ce6665946dd87f36b05d4da0
abdbashar <abdullahbashar98@gmail.com>
2023-11-22 21:50:28 +0300

    Update StudentDocsAdapter to include kebab menu item click handling for showing a popup menu.

--- app/src/main/java/com/example/huc_app/ui/studentDocs/StudentDocsAdapter.kt
+++ app/src/main/java/com/example/huc_app/ui/studentDocs/StudentDocsAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.huc_app.ui.studentDocs
 
+import android.view.View
 import com.example.huc_app.R
 import com.example.huc_app.ui.base.BaseAdapter
 import com.example.huc_app.ui.base.BaseInteractionListener
@@ -11,8 +12,15 @@ class StudentDocsAdapter(
 ) : BaseAdapter<DocumentUIState>(list, listener) {
     override val layoutID: Int = R.layout.item_document
 
+    override fun bind(holder: ItemViewHolder, position: Int) {
+        super.bind(holder, position)
 
+        holder.itemView.findViewById<View>(R.id.kebabMenu).setOnClickListener { view ->
+            showPopupMenu(view, getItemAt(position))
+        }
+    }
 }
+
 
 interface StudentDocsClicksListener : BaseInteractionListener {
     fun onListClick(item: DocumentUIState)
