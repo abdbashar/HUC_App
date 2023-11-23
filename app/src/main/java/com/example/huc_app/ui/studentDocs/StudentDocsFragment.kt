d50268be6816432818bb6a6d898522cb6e9730fb
abdbashar <abdullahbashar98@gmail.com>
2023-11-23 17:57:56 +0300

    Implement observeUIState function in StudentDocsFragment for updating UI with the latest student documents.

--- app/src/main/java/com/example/huc_app/ui/studentDocs/StudentDocsFragment.kt
+++ app/src/main/java/com/example/huc_app/ui/studentDocs/StudentDocsFragment.kt
@@ -1,18 +1,12 @@
 package com.example.huc_app.ui.studentDocs
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.view.animation.AnimationUtils.loadAnimation
-import androidx.activity.addCallback
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
 import com.example.huc_app.R
 import com.example.huc_app.databinding.FragmentStudentDocsBinding
 import com.example.huc_app.ui.base.BaseFragment
-import com.example.huc_app.util.*
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -23,12 +17,22 @@ class StudentDocsFragment : BaseFragment<FragmentStudentDocsBinding>() {
 
     override val viewModel: StudentDocsViewModel by viewModels()
 
+    private lateinit var studentDocsAdapter: StudentDocsAdapter
+
     override var bottomNavigationViewVisibility = View.GONE
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observeUIState()
     }
 
+    private fun observeUIState() {
+        lifecycleScope.launch {
+            viewModel.studentDocsUIState.collect {
+                it.studentDocs.let { studentDocs -> studentDocsAdapter.setItems(studentDocs.documents) }
+            }
+        }
+    }
 }
 
