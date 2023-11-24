0e7ad6d0b740f1b5123e9f35ad27ff007f7cc89d
abdbashar <abdullahbashar98@gmail.com>
2023-11-24 18:15:52 +0300

    Handle onListClick in StudentDocsViewModel to post the clicked DocumentUIState for further processing.

--- app/src/main/java/com/example/huc_app/ui/studentDocs/StudentDocsViewModel.kt
+++ app/src/main/java/com/example/huc_app/ui/studentDocs/StudentDocsViewModel.kt
@@ -1,10 +1,15 @@
 package com.example.huc_app.ui.studentDocs
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.huc_app.domain.useCases.CheckConnectivityUseCase
 import com.example.huc_app.domain.useCases.GetStudentDocumentsUseCase
 import com.example.huc_app.ui.studentDocs.studentDocsUIState.GetStudentDocsUIState
+import com.example.huc_app.ui.studentDocs.studentDocsUIState.DocumentUIState
+import com.example.huc_app.util.Event
+import com.example.huc_app.util.postEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -17,10 +22,13 @@ class StudentDocsViewModel @Inject constructor(
     private val getStudentDocumentsUseCase: GetStudentDocumentsUseCase,
     private val studentDocumentsUIMapper: StudentDocumentsUIMapper,
     private val checkConnectivityUseCase: CheckConnectivityUseCase,
-) : ViewModel() {
+) : ViewModel(), StudentDocsClicksListener {
 
     private val _studentDocsUIState = MutableStateFlow(GetStudentDocsUIState())
     val studentDocsUIState: StateFlow<GetStudentDocsUIState> get() = _studentDocsUIState
+
+    private val _studentDocsItemUIState = MutableLiveData<Event<DocumentUIState>>()
+    val studentDocsItemUIState: LiveData<Event<DocumentUIState>> get() = _studentDocsItemUIState
 
     init {
         getStudentDocs()
@@ -29,12 +37,7 @@ class StudentDocsViewModel @Inject constructor(
     private fun getStudentDocs() {
         viewModelScope.launch {
             if (checkConnectivityUseCase()) {
-                _studentDocsUIState.update {
-                    it.copy(
-                        isLoading = true,
-                        isInternetUnAvailable = false
-                    )
-                }
+                _studentDocsUIState.update { it.copy(isLoading = true, isInternetUnAvailable = false) }
                 try {
                     val studentDocuments =
                         studentDocumentsUIMapper.map(getStudentDocumentsUseCase())
@@ -56,13 +59,21 @@ class StudentDocsViewModel @Inject constructor(
                     }
                 }
             } else {
-                _studentDocsUIState.update {
-                    it.copy(
-                        isLoading = false,
-                        isInternetUnAvailable = true
-                    )
-                }
+                _studentDocsUIState.update { it.copy(isLoading = false, isInternetUnAvailable = true) }
             }
         }
     }
+
+    override fun onListClick(item: DocumentUIState) {
+        _studentDocsItemUIState.postEvent(item)
+    }
+
+    override fun onOpenDoc(item: DocumentUIState) {
+        TODO("Not implemented yet")
+    }
+
+    override fun onDownloadDoc(item: DocumentUIState) {
+        TODO("Not implemented yet")
+    }
+
 }
