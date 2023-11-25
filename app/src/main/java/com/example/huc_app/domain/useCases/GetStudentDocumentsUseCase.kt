988532c32e035148239b433a91b4263d492f1163
abdbashar <abdullahbashar98@gmail.com>
2023-11-25 20:20:01 +0300

    Implement invoke function in GetStudentDocumentsUseCase to get student docs from the remote data source

--- app/src/main/java/com/example/huc_app/domain/useCases/GetStudentDocumentsUseCase.kt
+++ app/src/main/java/com/example/huc_app/domain/useCases/GetStudentDocumentsUseCase.kt
@@ -2,10 +2,17 @@ package com.example.huc_app.domain.useCases
 
 import com.example.huc_app.data.repository.UserRepository
 import com.example.huc_app.domain.mappers.StudentDocumentsMapper
+import com.example.huc_app.domain.models.StudentDocuments
 import javax.inject.Inject
 
 class GetStudentDocumentsUseCase @Inject constructor(
     private val userRepository: UserRepository,
     private val studentDocumentsMapper: StudentDocumentsMapper
 ) {
+    suspend operator fun invoke(): StudentDocuments{
+        val response = userRepository.getStudentDocs()
+        return response.let { docs ->
+            studentDocumentsMapper.map(docs)
+        } ?: throw Throwable("Unable to get content")
+    }
 }
