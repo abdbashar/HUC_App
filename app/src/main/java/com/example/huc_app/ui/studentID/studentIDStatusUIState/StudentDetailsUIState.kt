d385f6cd2d0b03a9c19beb5a1e383bb1afb48db5
abdbashar <abdullahbashar98@gmail.com>
2023-11-27 21:35:34 +0300

    Created StudentDetailsUIState to display student details in the fragment_student_id

---
+++ app/src/main/java/com/example/huc_app/ui/studentID/studentIDStatusUIState/StudentDetailsUIState.kt
@@ -0,0 +1,10 @@
+package com.example.huc_app.ui.studentID.studentIDStatusUIState
+
+data class StudentDetailsUIState(
+    var fullName: String = "",
+    var personalPicture: String = "",
+    var departmentName: String = "",
+    var departmentNameInEnglish: String = "",
+    var stage: String = "",
+    var studySystemType: String = "",
+)
