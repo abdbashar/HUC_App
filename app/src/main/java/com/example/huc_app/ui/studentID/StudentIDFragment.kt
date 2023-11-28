103963558d64011a522cc0ff6571806c5d975f24
abdbashar <abdullahbashar98@gmail.com>
2023-11-28 19:18:50 +0300

    Observe current language in the observeEvents function to display info based on current language.

--- app/src/main/java/com/example/huc_app/ui/studentID/StudentIDFragment.kt
+++ app/src/main/java/com/example/huc_app/ui/studentID/StudentIDFragment.kt
@@ -5,20 +5,31 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import com.example.huc_app.R
 import com.example.huc_app.databinding.FragmentStudentIdBinding
+import com.example.huc_app.domain.types.Language
 import com.example.huc_app.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class StudentIDFragment : BaseFragment<FragmentStudentIdBinding>() {
-
     override fun getLayoutId(): Int = R.layout.fragment_student_id
 
     override val viewModel: StudentIDViewModel by viewModels()
 
     override var bottomNavigationViewVisibility = View.GONE
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observeEvents()
     }
 
+    private fun observeEvents() {
+        viewModel.currentLanguage.observe(viewLifecycleOwner) { language ->
+            if (language == Language.ARABIC) {
+                binding.departmentName.text =
+                    viewModel.studentIDStatus.value.studentDetails.departmentName
+            } else binding.departmentName.text =
+                viewModel.studentIDStatus.value.studentDetails.departmentNameInEnglish
+        }
+    }
 }
