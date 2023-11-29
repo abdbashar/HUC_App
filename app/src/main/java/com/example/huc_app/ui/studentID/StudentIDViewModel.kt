04d2cece44de36be2ca1c70eba850c7101c00c75
abdbashar <abdullahbashar98@gmail.com>
2023-11-29 12:56:44 +0300

    Implement getCurrentAppLanguage function

--- app/src/main/java/com/example/huc_app/ui/studentID/StudentIDViewModel.kt
+++ app/src/main/java/com/example/huc_app/ui/studentID/StudentIDViewModel.kt
@@ -1,9 +1,15 @@
 package com.example.huc_app.ui.studentID
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.huc_app.domain.types.Language
 import com.example.huc_app.util.SettingsService
 
 class StudentIDViewModel : ViewModel() {
+
+    private val _currentLanguage = MutableLiveData<Language>()
+    val currentLanguage: LiveData<Language> get() = _currentLanguage
 
     private val settingsService = SettingsService
 
@@ -12,7 +18,7 @@ class StudentIDViewModel : ViewModel() {
     }
 
     private fun getCurrentAppLanguage() {
-        TODO("Not implemented yet")
+        _currentLanguage.postValue(settingsService.getCurrentLanguage())
     }
 
 }
