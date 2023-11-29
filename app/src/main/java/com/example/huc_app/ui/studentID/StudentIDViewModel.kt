a1b9b4ac83e533c9ab580b666aa9ae45c6608793
abdbashar <abdullahbashar98@gmail.com>
2023-11-29 12:38:33 +0300

    Initialize getCurrentAppLanguage function to get current app language using SettingsService

--- app/src/main/java/com/example/huc_app/ui/studentID/StudentIDViewModel.kt
+++ app/src/main/java/com/example/huc_app/ui/studentID/StudentIDViewModel.kt
@@ -1,6 +1,18 @@
 package com.example.huc_app.ui.studentID
 
 import androidx.lifecycle.ViewModel
+import com.example.huc_app.util.SettingsService
 
 class StudentIDViewModel : ViewModel() {
+
+    private val settingsService = SettingsService
+
+    init {
+        getCurrentAppLanguage()
+    }
+
+    private fun getCurrentAppLanguage() {
+        TODO("Not implemented yet")
+    }
+
 }
