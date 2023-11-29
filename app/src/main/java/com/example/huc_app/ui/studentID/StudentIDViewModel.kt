596d21bf6f3cc51e43858e7e861711641fa5ba4f
abdbashar <abdullahbashar98@gmail.com>
2023-11-29 13:02:56 +0300

    Implement constructor injection for StudentIDViewModel using Dagger Hilt, including dependencies GetStudentDetailsUseCase and StudentDetailsUIMapper.

--- app/src/main/java/com/example/huc_app/ui/studentID/StudentIDViewModel.kt
+++ app/src/main/java/com/example/huc_app/ui/studentID/StudentIDViewModel.kt
@@ -4,9 +4,23 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.huc_app.domain.types.Language
+import com.example.huc_app.domain.useCases.GetStudentDetailsUseCase
+import com.example.huc_app.ui.studentID.studentIDStatusMapper.StudentDetailsUIMapper
+import com.example.huc_app.ui.studentID.studentIDStatusUIState.GetStudentIDStatusUIState
 import com.example.huc_app.util.SettingsService
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
 
-class StudentIDViewModel : ViewModel() {
+@HiltViewModel
+class StudentIDViewModel @Inject constructor(
+    private val getStudentDetailsUseCase: GetStudentDetailsUseCase,
+    private val studentDetailsUIMapper: StudentDetailsUIMapper,
+) : ViewModel() {
+
+    private val _studentIDStatus = MutableStateFlow(GetStudentIDStatusUIState())
+    val studentIDStatus: StateFlow<GetStudentIDStatusUIState> get() = _studentIDStatus
 
     private val _currentLanguage = MutableLiveData<Language>()
     val currentLanguage: LiveData<Language> get() = _currentLanguage
@@ -20,5 +34,4 @@ class StudentIDViewModel : ViewModel() {
     private fun getCurrentAppLanguage() {
         _currentLanguage.postValue(settingsService.getCurrentLanguage())
     }
-
 }
