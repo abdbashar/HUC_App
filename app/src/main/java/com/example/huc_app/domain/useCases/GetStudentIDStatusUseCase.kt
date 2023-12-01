662bae2e7f354c6e787878609ba9a3026ebec133
abdbashar <abdullahbashar98@gmail.com>
2023-12-01 09:48:33 +0300

    Implemented GetStudentIDStatusUseCase inside invoke function

--- app/src/main/java/com/example/huc_app/domain/useCases/GetStudentIDStatusUseCase.kt
+++ app/src/main/java/com/example/huc_app/domain/useCases/GetStudentIDStatusUseCase.kt
@@ -2,10 +2,17 @@ package com.example.huc_app.domain.useCases
 
 import com.example.huc_app.data.repository.UserRepository
 import com.example.huc_app.domain.mappers.StudentIDStatusMapper
+import com.example.huc_app.domain.models.StudentIDStatus
 import javax.inject.Inject
 
 class GetStudentIDStatusUseCase @Inject constructor(
     private val userRepository: UserRepository,
     private val studentIDStatusMapper: StudentIDStatusMapper,
 ) {
+    suspend operator fun invoke(): StudentIDStatus {
+        val response = userRepository.getStudentIDStatus()
+        return response.let { studentID ->
+            studentIDStatusMapper.map(studentID)
+        } ?: throw Throwable("Unable to get content")
+    }
 }
