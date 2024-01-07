5763adf8e1e73d6b86c9498c3ad39abf98a8556a
abdbashar <abdullahbashar98@gmail.com>
2024-01-07 13:41:48 +0300

    Observe student fees and set it to the PaymentAdapter

--- app/src/main/java/com/example/huc_app/ui/bills/BillsFragment.kt
+++ app/src/main/java/com/example/huc_app/ui/bills/BillsFragment.kt
@@ -4,10 +4,12 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.huc_app.R
 import com.example.huc_app.databinding.FragmentBillsBinding
 import com.example.huc_app.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
@@ -19,6 +21,8 @@ class BillsFragment : BaseFragment<FragmentBillsBinding>() {
 
     override var bottomNavigationViewVisibility = View.GONE
 
+    private lateinit var paymentAdapter: PaymentAdapter
+
     private var originalStatusBarColor: Int = 0
 
 
@@ -26,10 +30,25 @@ class BillsFragment : BaseFragment<FragmentBillsBinding>() {
         super.onViewCreated(view, savedInstanceState)
         originalStatusBarColor = requireActivity().window.statusBarColor
         requireActivity().window.statusBarColor = Color.WHITE
+        setAdapter()
+        observeUIState()
     }
 
     override fun onDestroyView() {
         requireActivity().window.statusBarColor = originalStatusBarColor
         super.onDestroyView()
+    }
+
+    private fun observeUIState() {
+        lifecycleScope.launch {
+            viewModel.studentFeesUIState.collect {
+                it.studentFees.payments.let { studentPayments -> paymentAdapter.setItems(studentPayments) }
+            }
+        }
+    }
+
+    private fun setAdapter() {
+        paymentAdapter = PaymentAdapter(emptyList(), viewModel)
+        binding.studentPayments.adapter = paymentAdapter
     }
 }
