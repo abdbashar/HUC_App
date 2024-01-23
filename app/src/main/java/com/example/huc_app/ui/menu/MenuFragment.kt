4d0e919692288435717720d38eab68c9fe4deea3
abdbashar <abdullahbashar98@gmail.com>
2024-01-23 11:27:47 +0300

    Added navigation event to the pay fees screen

--- app/src/main/java/com/example/huc_app/ui/menu/MenuFragment.kt
+++ app/src/main/java/com/example/huc_app/ui/menu/MenuFragment.kt
@@ -25,13 +25,14 @@ class MenuFragment : BaseFragment<FragmentMenuBindingImpl>() {
     private fun observeEvents() {
         viewModel.menuEvent.observeEvent(viewLifecycleOwner){ event ->
             when (event) {
-                 MenuEvent.PersonalInfoClicked -> navigateToScreenById(R.id.action_menuFragment_to_personalInfoFragment)
+                MenuEvent.PersonalInfoClicked -> navigateToScreenById(R.id.action_menuFragment_to_personalInfoFragment)
                 MenuEvent.SecondarySchoolInfoClicked -> navigateToScreenById(R.id.action_menuFragment_to_secondarySchoolInfoFragment)
                 MenuEvent.UniversityInfoClicked -> navigateToScreenById(R.id.action_menuFragment_to_universityInfoFragment)
                 MenuEvent.StudentDocsClicked -> navigateToScreenById(R.id.action_menuFragment_to_studentDocsFragment)
                 MenuEvent.StudentIDClicked -> navigateToScreenById(R.id.action_menuFragment_to_studentIDFragment)
                 MenuEvent.SubmitRequestsClicked -> navigateToScreenById(R.id.action_menuFragment_to_requestsFragment)
                 MenuEvent.BillsClicked -> navigateToScreenById(R.id.action_menuFragment_to_billsFragment)
+                MenuEvent.PayFeesClicked -> navigateToScreenById(R.id.action_menuFragment_to_payFeesFragment)
             }
         }
     }
